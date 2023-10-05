d5f88a66f020aed865af9fe0ce1a30b2c54734f8
kuzmix <sk804475@gmail.com>
2023-10-05 18:28:59 +0300

    Add to summarize_grouper_fragment 'range' method Add find_pattern method

--- battery_parser/statistics.py
+++ battery_parser/statistics.py
@@ -4,51 +4,26 @@ This module will provide functions for splitting, parsing battery CC/CV cycles f
 import pandas as pd
 
 
-class Check_pattern:  # TODO перенести в файл с обработкой (который будет)
-    """
-    Creates object that check if given
-    Args:
-        pattern:str string of '+', '-' or '0' for positive, negative and zero value
-    """
-
-    def __init__(self, pattern):
-        self.pattern = pattern
-
-    def __call__(self, split: list):
-        assert len(self.pattern) == len(split)
-        checks = [self.check_element(element, condition)
-                  for element, condition
-                  in zip(split, self.pattern)]
-        return all(checks)
-
-    @staticmethod
-    def check_element(element, condition):
-        if condition == '0':
-            return element == 0
-        elif condition == '+':
-            return element > 0
-        elif condition == "-":
-            return element < 0
-
-
 def generate_statistics(data: pd.DataFrame,
                         group_marker="Step",
-                        statistics_pattern={'I': ['mean', 'std'],
-                                            'Time': ['max', 'diff'],
-                                            'E': 'mean',
-                                            'T': ['min', 'max']}):
+                        statistics_pattern={'I':['mean', 'std'],
+                                            'Time':['range', 'diff'],
+                                            'E':'mean',
+                                            'T':['min', 'max']}):
     """
     Group dataframe by 'group_marker' and summarize explicit columns with given methods.
     'statistic_pattern' configure statistic results.
     Standard pattern include Current, time and voltage summarization.
-    Summarization methods are currently 'mean', 'std', 'max', 'min'.
+    Summarization methods are currently 'mean', 'std', 'max', 'min', 'diff', 'range', 'count',
+            'unique_values'.
     Args:
         data (pd.Dataframe): cycle/experiment/etc dataframe.
         group_marker (str or list[str]): column (columns) which will be used as group marker
                                         (argument for df.groupby() )
         statistics_pattern (dict): str:str, where column is column name you want to summarize,
                                     and value is method you want to use for summarization
-                                    (mean, std, max, min)
+                                    ('mean', 'std', 'max', 'min', 'diff', 'count',
+                                            'unique_values', 'range')
 
     Returns: pd.Dataframe, with total statistics via marker.
 
@@ -90,7 +65,7 @@ def column_statistics_step(grouped_data: pd.DataFrame, column: str, method: str)
     Args:
         grouped_data (): group object (or just Dataframe maybe) for summarization
         column (str): Column name
-        method (str): method name: min, max, mean, std, diff
+        method (str): method name: min, max, mean, std, diff, 'range'
 
     Returns:
             (dict) with one entry - statistic summary for method and column. Key = column_method
@@ -106,7 +81,7 @@ def summarize_grouper_fragment(grouped_slice, method: str = 'mean'):
     Args:
         grouped_slice (pd.Series.groupby): grouper object for one Series
         method (str): method marker. Handle 'mean', 'std', 'max', 'min', 'diff', 'count',
-        'unique_values' methods
+        'unique_values', 'range' methods
 
     Returns:
         pd.Series
@@ -120,10 +95,12 @@ def summarize_grouper_fragment(grouped_slice, method: str = 'mean'):
             return grouped_slice.max()
         case 'min':
             return grouped_slice.min()
+        case 'range':
+            return grouped_slice.max() - grouped_slice.min()
         case 'diff':
             index_name = grouped_slice.keys
             column_name = grouped_slice.nth(0).name
-            dict_diff = {step[0]: step[1].diff().mean() for step in grouped_slice}
+            dict_diff = {step[0]:step[1].diff().mean() for step in grouped_slice}
             output = pd.Series(dict_diff, name=column_name)
             output.index.name = index_name
             return output
@@ -132,10 +109,61 @@ def summarize_grouper_fragment(grouped_slice, method: str = 'mean'):
         case 'unique_values':
             if all(grouped_slice.nunique() > 10):
                 print('Warning! More than 10 unique values in group!')
-                return grouped_slice.unique().apply(transform_np_to_str)
+            return grouped_slice.unique().apply(transform_np_to_str)
         case _:
             raise ValueError
 
 
 def transform_np_to_str(element):
     return ', '.join(element.tolist())
+
+
+class Check_pattern:  # TODO перенести в файл с обработкой (который будет)
+    """
+    Creates object that checks if given value list corresponds to given pattern.
+    Args:
+        pattern:str string of '+', '-' or '0' for positive, negative and zero value
+    """
+
+    def __init__(self, pattern):
+        self.pattern = pattern
+
+    def __call__(self, split: list):
+        """
+        Checks if values in list are consistent with pattern. List length should be
+        equal to pattern length.
+        Args:
+            split (): list of values.
+
+        Returns:
+
+        """
+        assert len(self.pattern) == len(split)
+        checks = [self.check_element(element, condition)
+                  for element, condition
+                  in zip(split, self.pattern)]
+        return all(checks)
+
+    def __len__(self):
+        return len(self.pattern)
+
+    @staticmethod
+    def check_element(element, condition):
+        if condition == '0':
+            return element == 0
+        elif condition == '+':
+            return element > 0
+        elif condition == "-":
+            return element < 0
+
+
+def find_pattern(statistics: pd.Series, pattern: str):
+    pattern_checker = Check_pattern(pattern)
+    window_size = len(pattern_checker)
+    roll = statistics.rolling(window_size).apply(pattern_checker)
+    windows = []
+    for i in roll.index[roll == 1]:
+        end_index = i + 1
+        start_index = end_index - window_size
+        windows.append(list(range(start_index, end_index)))
+    return windows
